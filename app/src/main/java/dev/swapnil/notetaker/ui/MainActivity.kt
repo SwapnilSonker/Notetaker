91c9f062930eed6d363bcbd00c253d01b8155ffb
SwapnilSonker <swapnilsonkarcse2019@bbdu.ac.in>
2023-01-28 15:39:11 +0530

    Added splashscreen

--- app/src/main/java/dev/swapnil/notetaker/ui/MainActivity.kt
+++ app/src/main/java/dev/swapnil/notetaker/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.swapnil.notetaker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
@@ -14,7 +15,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //val splashScreen = installSplashScreen()
+
+
         setContentView(R.layout.activity_main)
+
+
+
+
 
         var navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
           navController = navHostFragment.navController
---
+++ app/src/main/java/dev/swapnil/notetaker/ui/SplashScreen.kt
@@ -0,0 +1,19 @@
+package dev.swapnil.notetaker.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import dev.swapnil.notetaker.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this,MainActivity::class.java))
+        },5000)
+    }
+}
